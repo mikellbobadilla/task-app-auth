7ebc8290cb16ac67883ab543fee8ed8c77ee27d8
Sting <bobadillasting@gmail.com>
2024-03-16 16:58:50 -0300

    Add AccountController for creating accounts

---
+++ src/main/java/ar/mikellbobadilla/controller/AccountController.java
@@ -0,0 +1,27 @@
+package ar.mikellbobadilla.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ar.mikellbobadilla.dto.AccountRequest;
+import ar.mikellbobadilla.dto.AccountResponse;
+import ar.mikellbobadilla.service.AccountService;
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/accounts")
+@AllArgsConstructor
+public class AccountController {
+
+    private final AccountService service;
+
+    @PostMapping
+    ResponseEntity<AccountResponse> createAccount(@RequestBody AccountRequest request) {
+        return new ResponseEntity<>(service.create(request), HttpStatus.CREATED);
+    }
+
+}
