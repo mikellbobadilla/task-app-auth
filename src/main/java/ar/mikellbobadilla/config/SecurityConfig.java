919c2efb4c323470cf452867acf4bbbf5c09af31
Sting <bobadillasting@gmail.com>
2024-03-16 16:58:34 -0300

    Add security configurations and filter chain

--- src/main/java/ar/mikellbobadilla/config/SecurityConfig.java
+++ src/main/java/ar/mikellbobadilla/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package ar.mikellbobadilla.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,17 +21,18 @@ public class SecurityConfig {
     private final AuthenticationProvider provider;
     private final JwtFilter jwtFilter;
 
+    @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(httpRequest -> {
-                    httpRequest.requestMatchers("/api/auth/**").permitAll();
-                    httpRequest.requestMatchers("/api/todos/**").authenticated();
+                    httpRequest.requestMatchers("/api/auth/**", "/h2-console/**").permitAll();
+                    httpRequest.requestMatchers(HttpMethod.POST, "/api/accounts/**").permitAll();
+                    httpRequest.requestMatchers("/api/tasks/**").authenticated();
                     httpRequest.anyRequest().denyAll();
                 })
                 .sessionManagement(management -> {
-
                     management.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
                 })
                 .authenticationProvider(provider)
