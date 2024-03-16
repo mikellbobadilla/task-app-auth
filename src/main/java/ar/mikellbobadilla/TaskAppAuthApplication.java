fd222a6fccc5a3599b5b86291a9c51a31277766d
Sting <bobadillasting@gmail.com>
2024-03-15 22:33:16 -0300

    Initial commit

---
+++ src/main/java/ar/mikellbobadilla/TaskAppAuthApplication.java
@@ -0,0 +1,13 @@
+package ar.mikellbobadilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAppAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskAppAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/mikellbobadilla/TaskAppAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.mikellbobadilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAppAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
