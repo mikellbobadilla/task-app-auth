83641c8dbd4f8c3684332c6f84fc9a592fe1ff10
Sting <bobadillasting@gmail.com>
2024-03-17 19:37:35 -0300

    Add TaskAdvice class for handling exceptions

---
+++ src/main/java/ar/mikellbobadilla/advice/TaskAdvice.java
@@ -0,0 +1,34 @@
+package ar.mikellbobadilla.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import ar.mikellbobadilla.exception.TaskException;
+import ar.mikellbobadilla.exception.TaskNotFoundException;
+import lombok.AllArgsConstructor;
+
+@RestControllerAdvice
+@AllArgsConstructor
+public class TaskAdvice {
+
+    @ExceptionHandler(TaskException.class)
+    ResponseEntity<ErrorResponse> taskException(TaskException exc) {
+
+        ErrorResponse res = ErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .error(exc.getMessage())
+                .build();
+        return new ResponseEntity<>(res, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    ResponseEntity<ErrorResponse> taskNotFoundException(TaskNotFoundException exc) {
+        ErrorResponse res = ErrorResponse.builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .error(exc.getMessage())
+                .build();
+        return new ResponseEntity<>(res, HttpStatus.NOT_FOUND);
+    }
+}
