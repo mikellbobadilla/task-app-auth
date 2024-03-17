531b96271886790b8a01b1e8250375d6efe2947c
Sting <bobadillasting@gmail.com>
2024-03-17 20:17:37 -0300

    Add AccountRepositoryTest class for testing AccountRepository

---
+++ src/test/java/ar/mikellbobadilla/repository/AccountRepositoryTest.java
@@ -0,0 +1,120 @@
+package ar.mikellbobadilla.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import ar.mikellbobadilla.model.Account;
+import jakarta.transaction.Transactional;
+
+@DataJpaTest
+@Transactional(Transactional.TxType.NOT_SUPPORTED)
+public class AccountRepositoryTest {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(accountRepository);
+    }
+
+    @Test
+    void verifyPersistModel() {
+        Account account = createAccount();
+        assertNull(account.getId());
+        accountRepository.save(account);
+        assertNotNull(account.getId());
+    }
+
+    @Test
+    void verifyFindAccountByUsernameAndExists() {
+        Account account = createAccount();
+        accountRepository.save(account);
+        Optional<Account> accountOptional = accountRepository.findByUsername(account.getUsername());
+        assertNotNull(accountOptional.get());
+    }
+
+    @Test
+    void verifyFindAccountByUsernameAndNotExists() {
+        Optional<Account> accountOptional = accountRepository.findByUsername("username");
+        assertTrue(accountOptional.isEmpty());
+    }
+
+    @Test
+    void verifyExistsUsernameAccount() {
+        Account account = createAccount();
+        accountRepository.save(account);
+        boolean exists = accountRepository.existsByUsername(account.getUsername());
+        assertTrue(exists);
+    }
+
+    @Test
+    void verifyNoExistsUsernameAccount() {
+        boolean exists = accountRepository.existsByUsername("username");
+        assertFalse(exists);
+    }
+
+    @Test
+    void verifyExistsUsernameAccountAndIdNot() {
+        Account account = createAccount();
+        Account secondAccount = Account.builder()
+                .username("username2")
+                .password("password")
+                .build();
+        accountRepository.save(account);
+        accountRepository.save(secondAccount);
+
+        boolean exists = accountRepository.existsByUsernameAndIdNot("username", secondAccount.getId());
+        assertTrue(exists);
+    }
+
+    @Test
+    void verifyNoExistsUsernameAccountAndIdNot() {
+        Account account = createAccount();
+        Account secondAccount = Account.builder()
+                .username("username2")
+                .password("password")
+                .build();
+        accountRepository.save(account);
+        accountRepository.save(secondAccount);
+
+        boolean exists = accountRepository.existsByUsernameAndIdNot("user", secondAccount.getId());
+        assertFalse(exists);
+    }
+
+    @Test
+    void verifyUpdateAccount() {
+        Account account = createAccount();
+        accountRepository.save(account);
+        assertNotNull(account.getId());
+        account.setUsername("user");
+        accountRepository.save(account);
+        assertEquals("user", account.getUsername());
+    }
+
+    @Test
+    void verifyRemoveAccount() {
+        Account account = createAccount();
+        accountRepository.save(account);
+        assertNotNull(account.getId());
+        accountRepository.deleteById(account.getId());
+        boolean exits = accountRepository.existsById(account.getId());
+        assertFalse(exits);
+    }
+
+    private Account createAccount() {
+        Account account = new Account();
+        account.setUsername("username");
+        account.setPassword("password");
+        return account;
+    }
+}
