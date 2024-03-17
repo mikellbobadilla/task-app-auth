f18a0737cca9c98bf5827b9d4544650af63ec677
sting <bobadillasting@gmail.com>
2024-03-17 00:08:57 -0300

    Add AuthController for authentication

---
+++ src/main/java/ar/mikellbobadilla/controller/AuthController.java
@@ -0,0 +1,27 @@
+package ar.mikellbobadilla.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ar.mikellbobadilla.dto.AuthRequest;
+import ar.mikellbobadilla.dto.AuthResponse;
+import ar.mikellbobadilla.exception.AccountException;
+import ar.mikellbobadilla.service.AuthService;
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
+public class AuthController {
+    
+    private final AuthService service;
+
+    @PostMapping
+    ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest request) throws AccountException {
+        return new ResponseEntity<>(service.authenticate(request), HttpStatus.OK);
+    }
+}
