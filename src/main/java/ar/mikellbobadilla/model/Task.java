9ecfa2107f4d0011d0e7d1b2d5b80888c37fe447
Sting <bobadillasting@gmail.com>
2024-03-17 15:49:08 -0300

    Add Task model class

---
+++ src/main/java/ar/mikellbobadilla/model/Task.java
@@ -0,0 +1,40 @@
+package ar.mikellbobadilla.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "db_tasks")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private boolean isDone;
+    @Column(nullable = false)
+    private Date targetDate;
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = Account.class)
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+}
