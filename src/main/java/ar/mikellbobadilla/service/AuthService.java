0043666ed433e5da2a3a0cec7a79cbe931795bce
sting <bobadillasting@gmail.com>
2024-03-17 00:09:20 -0300

    Add AuthService class for authentication

---
+++ src/main/java/ar/mikellbobadilla/service/AuthService.java
@@ -0,0 +1,34 @@
+package ar.mikellbobadilla.service;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import ar.mikellbobadilla.dto.AuthRequest;
+import ar.mikellbobadilla.dto.AuthResponse;
+import ar.mikellbobadilla.exception.AccountException;
+import ar.mikellbobadilla.model.Account;
+import ar.mikellbobadilla.repository.AccountRepository;
+import ar.mikellbobadilla.service.interfaces.JwtService;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    private final AuthenticationManager manager;
+    private final JwtService jwtService;
+    private final AccountRepository accountRepository;
+
+    public AuthResponse authenticate(AuthRequest request) throws AccountException {
+        Authentication auth = new UsernamePasswordAuthenticationToken(request.username(), request.password());
+
+        manager.authenticate(auth);
+
+        Account account = accountRepository.findByUsername(request.username())
+                .orElseThrow(() -> new AccountException("Bad credentials!"));
+
+        return new AuthResponse(account.getUsername(), jwtService.createToken(account));
+    }
+}
