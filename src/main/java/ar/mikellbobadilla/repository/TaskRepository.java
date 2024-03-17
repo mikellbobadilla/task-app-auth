4add908f3937c6adfff522a8bd81ca50ce8f8e8b
Sting <bobadillasting@gmail.com>
2024-03-17 19:38:02 -0300

    Add findAllByAccountId method to TaskRepository

--- src/main/java/ar/mikellbobadilla/repository/TaskRepository.java
+++ src/main/java/ar/mikellbobadilla/repository/TaskRepository.java
@@ -1,9 +1,13 @@
 package ar.mikellbobadilla.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ar.mikellbobadilla.model.Task;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    Page<Task> findAllByAccountId(Long id, Pageable pageable);
 
 }
