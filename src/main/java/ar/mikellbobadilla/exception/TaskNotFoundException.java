1a284b43dfde6fe07719d03799d1575ff2e17e8d
Sting <bobadillasting@gmail.com>
2024-03-17 19:37:51 -0300

    Add TaskNotFoundException class

---
+++ src/main/java/ar/mikellbobadilla/exception/TaskNotFoundException.java
@@ -0,0 +1,8 @@
+package ar.mikellbobadilla.exception;
+
+public class TaskNotFoundException extends TaskException {
+
+    public TaskNotFoundException(String message) {
+        super(message);
+    }
+}
