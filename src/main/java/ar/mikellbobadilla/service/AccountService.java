ef2f504a4d94a73799c2ddb00f08d0bc2a7deb93
sting <bobadillasting@gmail.com>
2024-03-16 23:09:50 -0300

    Add updateUsernameAccount method to AccountService

--- src/main/java/ar/mikellbobadilla/service/AccountService.java
+++ src/main/java/ar/mikellbobadilla/service/AccountService.java
@@ -1,10 +1,17 @@
 package ar.mikellbobadilla.service;
 
+import java.security.Security;
+
+import javax.security.auth.login.AccountNotFoundException;
+
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import ar.mikellbobadilla.dto.AccountRequest;
 import ar.mikellbobadilla.dto.AccountResponse;
+import ar.mikellbobadilla.dto.UpdateUsernameRequest;
 import ar.mikellbobadilla.exception.AccountException;
 import ar.mikellbobadilla.model.Account;
 import ar.mikellbobadilla.repository.AccountRepository;
@@ -31,13 +38,21 @@ public class AccountService {
                 .username(request.username())
                 .password(encoder.encode(request.password()))
                 .build();
-                
+
         return parseToAccontResponse(repository.saveAndFlush(account));
     }
 
+    public AccountResponse updateUsernameAccount(String username, UpdateUsernameRequest request) throws AccountNotFoundException {
 
+        Account account = repository.findByUsername(username)
+                .orElseThrow(() -> new AccountNotFoundException("Account not found!"));
 
-    private AccountResponse  parseToAccontResponse(Account account) {
+        System.out.println("Account: " + account);
+        
+        return parseToAccontResponse(account);
+    }
+
+    private AccountResponse parseToAccontResponse(Account account) {
         return new AccountResponse(account.getId(), account.getUsername());
     }
 }
