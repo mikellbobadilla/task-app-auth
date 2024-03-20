73ab37bd8620b4e55c869f8f13fdd30e92a74cfa
Sting <bobadillasting@gmail.com>
2024-03-19 22:28:00 -0300

    Refactor account controller to use AccountUtil and update endpoint paths

--- src/main/java/ar/mikellbobadilla/controller/AccountController.java
+++ src/main/java/ar/mikellbobadilla/controller/AccountController.java
@@ -5,13 +5,13 @@ import ar.mikellbobadilla.dto.AccountResponse;
 import ar.mikellbobadilla.dto.ChangePasswordRequest;
 import ar.mikellbobadilla.dto.ChangeUsernameRequest;
 import ar.mikellbobadilla.exception.AccountException;
+import ar.mikellbobadilla.model.Account;
 import ar.mikellbobadilla.service.interfaces.AccountService;
+import ar.mikellbobadilla.utils.AccountUtil;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.security.auth.login.AccountNotFoundException;
 
 @RestController
 @RequestMapping("/api/accounts")
@@ -20,25 +20,45 @@ public class AccountController {
 
     private final AccountService service;
 
+    @GetMapping
+    ResponseEntity<AccountResponse> getAccount() throws AccountException {
+        Account account = AccountUtil.getAccountFromContext();
+        AccountResponse response;
+        response = service.getAccount(account.getId());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
     @PostMapping
     ResponseEntity<AccountResponse> createAccount(@RequestBody AccountRequest request) throws AccountException {
-        return new ResponseEntity<>(service.createAccount(request), HttpStatus.CREATED);
+        AccountResponse response;
+        response = service.createAccount(request);
+
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
-    @PatchMapping("/{id}/username")
-    ResponseEntity<AccountResponse> updateUsername(@PathVariable Long id, @RequestBody ChangeUsernameRequest request) throws AccountNotFoundException, AccountException {
-        return new ResponseEntity<>(service.updateUsername(id, request), HttpStatus.OK);
+    @PatchMapping("/username")
+    ResponseEntity<AccountResponse> updateUsername(@RequestBody ChangeUsernameRequest request) throws AccountException {
+        Account account = AccountUtil.getAccountFromContext();
+        AccountResponse response;
+        response = service.updateUsername(account.getId(), request);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @PatchMapping("/{id}/password")
-    ResponseEntity<Void> updatePassword(@PathVariable Long id, @RequestBody ChangePasswordRequest request) throws AccountNotFoundException, AccountException {
-        service.updatePassword(id, request);
+    @PatchMapping("/password")
+    ResponseEntity<Void> updatePassword(@RequestBody ChangePasswordRequest request) throws AccountException {
+        Account account = AccountUtil.getAccountFromContext();
+        service.updatePassword(account.getId(), request);
+
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @DeleteMapping("/{id}")
-    ResponseEntity<Void> deleteAccount(@PathVariable Long id, @RequestBody String password) throws AccountNotFoundException, AccountException {
-        service.deleteAccount(id, password);
+    @DeleteMapping
+    ResponseEntity<Void> deleteAccount(@RequestBody String password) throws AccountException {
+        Account account = AccountUtil.getAccountFromContext();
+        service.deleteAccount(account.getId(), password);
+
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
