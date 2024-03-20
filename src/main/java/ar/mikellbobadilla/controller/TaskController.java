0856c69466f6230b62a1de2420e02174f27ef054
Sting <bobadillasting@gmail.com>
2024-03-20 18:48:17 -0300

    Refactor TaskController to include authentication and update method signatures

--- src/main/java/ar/mikellbobadilla/controller/TaskController.java
+++ src/main/java/ar/mikellbobadilla/controller/TaskController.java
@@ -1,16 +1,17 @@
 package ar.mikellbobadilla.controller;
 
+import ar.mikellbobadilla.dto.PageResponse;
 import ar.mikellbobadilla.dto.TaskRequest;
 import ar.mikellbobadilla.dto.TaskResponse;
-import ar.mikellbobadilla.exception.AccountException;
-import ar.mikellbobadilla.exception.AccountNotFoundException;
+import ar.mikellbobadilla.exception.TaskException;
 import ar.mikellbobadilla.exception.TaskNotFoundException;
-import ar.mikellbobadilla.model.Task;
-import ar.mikellbobadilla.service.TaskService;
+import ar.mikellbobadilla.model.Account;
+import ar.mikellbobadilla.service.interfaces.TaskService;
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,39 +22,63 @@ public class TaskController {
     private final TaskService service;
 
     @GetMapping
-    ResponseEntity<Page<Task>> getTasks(
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size) throws AccountNotFoundException {
-        return new ResponseEntity<>(service.getTasks(page, size), HttpStatus.OK);
+    ResponseEntity<PageResponse<TaskResponse>> getTasks(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size) throws TaskException {
+
+        Account account = getAuthentication();
+
+        var response = service.getTasks(page, size, account.getId());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    ResponseEntity<TaskResponse> getTaskById(Long id) throws AccountException, TaskNotFoundException {
-        return new ResponseEntity<>(service.getTaskById(id), HttpStatus.OK);
+    ResponseEntity<TaskResponse> getTaskById(Long id) throws TaskNotFoundException {
+
+        Account account = getAuthentication();
+
+        return new ResponseEntity<>(service.getTaskById(id, account.getId()), HttpStatus.OK);
     }
 
     @PostMapping
-    ResponseEntity<TaskResponse> create(@RequestBody TaskRequest request) throws AccountException {
-        return new ResponseEntity<>(service.createTask(request), HttpStatus.CREATED);
+    ResponseEntity<TaskResponse> create(@RequestBody TaskRequest request) throws TaskException {
+
+        Account account = getAuthentication();
+
+        return new ResponseEntity<>(service.createTask(request, account.getId()), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     ResponseEntity<Void> updateTask(@PathVariable Long id, @RequestBody TaskRequest request)
-            throws TaskNotFoundException, IllegalAccessException {
-        service.updateTask(id, request);
+            throws TaskException, IllegalAccessException {
+
+        Account account = getAuthentication();
+
+        service.updateTaskFull(id, request, account.getId());
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PatchMapping("/{id}")
     ResponseEntity<TaskResponse> updateTaskStatus(@PathVariable Long id, @RequestBody TaskRequest request)
-            throws TaskNotFoundException, IllegalAccessException {
+            throws TaskException, IllegalAccessException {
 
-        return new ResponseEntity<>(service.updatePartialTask(id, request), HttpStatus.OK);
+        Account account = getAuthentication();
+
+        return new ResponseEntity<>(service.updateTaskPartial(id, request, account.getId()), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    ResponseEntity<Void> deleteTask(@PathVariable Long id) throws TaskNotFoundException, IllegalAccessException {
-        service.deleteTask(id);
+    ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+
+        Account account = getAuthentication();
+
+        service.deleteTask(id, account.getId());
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    private Account getAuthentication() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return (Account) auth.getPrincipal();
     }
 }
