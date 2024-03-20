123753ebb758368eecca4d84c5f0c38cc3be8385
Sting <bobadillasting@gmail.com>
2024-03-20 18:48:03 -0300

    Add methods to update password in ChangePasswordRequest

--- src/main/java/ar/mikellbobadilla/dto/ChangePasswordRequest.java
+++ src/main/java/ar/mikellbobadilla/dto/ChangePasswordRequest.java
@@ -4,4 +4,12 @@ public record ChangePasswordRequest(
         String password,
         String newPassword,
         String confirmNewPassword) {
+
+    public ChangePasswordRequest withNewPasswordAndConfirmNewPassword(String newPassword, String confirmNewPassword) {
+        return new ChangePasswordRequest(password, newPassword, confirmNewPassword);
+    }
+
+    public ChangePasswordRequest withPassword(String password) {
+        return new ChangePasswordRequest(password, newPassword, confirmNewPassword);
+    }
 }
