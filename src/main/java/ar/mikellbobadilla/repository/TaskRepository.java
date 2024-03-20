84d6edb170d345f436524b96c37689f2a2cdbead
Sting <bobadillasting@gmail.com>
2024-03-19 22:27:08 -0300

    Add findByIdAndAccountId and deleteByIdAndAccountId methods to TaskRepository

--- src/main/java/ar/mikellbobadilla/repository/TaskRepository.java
+++ src/main/java/ar/mikellbobadilla/repository/TaskRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
     Page<Task> findAllByAccountId(Long id, Pageable pageable);
 
+    Optional<Task> findByIdAndAccountId(Long id, Long accountId);
+
+    void deleteByIdAndAccountId(Long id, Long accountId);
 }
