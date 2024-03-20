e87c59d58d4bdbb63d7b506c7ee5a24cb563f448
Sting <bobadillasting@gmail.com>
2024-03-19 22:25:59 -0300

    Update TaskService interface with new method signatures

--- src/main/java/ar/mikellbobadilla/service/interfaces/TaskService.java
+++ src/main/java/ar/mikellbobadilla/service/interfaces/TaskService.java
@@ -1,23 +1,22 @@
 package ar.mikellbobadilla.service.interfaces;
 
+import ar.mikellbobadilla.dto.PageResponse;
 import ar.mikellbobadilla.dto.TaskRequest;
 import ar.mikellbobadilla.dto.TaskResponse;
 import ar.mikellbobadilla.exception.TaskException;
 import ar.mikellbobadilla.exception.TaskNotFoundException;
-import ar.mikellbobadilla.model.Task;
-import org.springframework.data.domain.Page;
 
 public interface TaskService {
 
-    Page<Task> getTasks(int page, int size, Long accountId);
+    PageResponse<TaskResponse> getTasks(int page, int size, Long accountId) throws TaskException;
 
     TaskResponse getTaskById(Long id, Long accountId) throws TaskNotFoundException;
 
     TaskResponse createTask(TaskRequest request, Long accountId) throws TaskException;
 
-    void updateTaskFull(Long id, TaskRequest request, Long accountId) throws TaskException;
+    void updateTaskFull(Long id, TaskRequest request, Long accountId) throws TaskException, IllegalAccessException;
 
-    TaskResponse updateTaskPartial(Long id, TaskRequest request, Long accountId) throws TaskException;
+    TaskResponse updateTaskPartial(Long id, TaskRequest request, Long accountId) throws TaskException, IllegalAccessException;
 
     void deleteTask(Long id, Long accountId);
 }
