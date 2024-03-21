cd4b2e6ff417f8a5e257a79db4573ee88c8e3027
Sting <bobadillasting@gmail.com>
2024-03-21 18:11:26 -0300

    Update SecurityConfig and application.properties

--- src/main/java/ar/mikellbobadilla/config/SecurityConfig.java
+++ src/main/java/ar/mikellbobadilla/config/SecurityConfig.java
@@ -2,15 +2,23 @@ package ar.mikellbobadilla.config;
 
 import ar.mikellbobadilla.filters.JwtFilter;
 import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @AllArgsConstructor
 @Configuration
@@ -19,18 +27,23 @@ public class SecurityConfig {
 
     private final AuthenticationProvider provider;
     private final JwtFilter jwtFilter;
+    @Value("${allowed.origin}")
+    private String origin;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors(cors -> Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(httpRequest -> {
-                    httpRequest.requestMatchers("/api/auth/**", "/h2-console/**").permitAll();
+                    httpRequest.requestMatchers("/api/auth/**").permitAll();
                     httpRequest.requestMatchers(HttpMethod.POST, "/api/accounts").permitAll();
-                    httpRequest.requestMatchers(HttpMethod.PATCH, "/api/accounts/*/username").authenticated();
-                    httpRequest.requestMatchers(HttpMethod.PATCH, "/api/accounts/*/password").authenticated();
+                    httpRequest.requestMatchers(HttpMethod.GET, "/api/accounts/**").authenticated();
+                    httpRequest.requestMatchers(HttpMethod.PATCH, "/api/accounts/username").authenticated();
+                    httpRequest.requestMatchers(HttpMethod.PATCH, "/api/accounts/password").authenticated();
                     httpRequest.requestMatchers(HttpMethod.DELETE, "/api/accounts/*").authenticated();
+                    httpRequest.requestMatchers("/h2-console/**").authenticated();
                     httpRequest.requestMatchers("/api/tasks/**").authenticated();
                     httpRequest.anyRequest().denyAll();
                 })
@@ -41,6 +54,17 @@ public class SecurityConfig {
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(origin));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTION"));
+        configuration.setAllowedHeaders(List.of("Authorization"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
